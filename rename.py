d809a836cb7d3c75b2b5a9161d9e93dd8182fd7b
RoyXue812 <royxue812@gmail.com>
2015-05-25 22:26:32 +0800

    deal with dataset name

---
+++ rename.py
@@ -0,0 +1,26 @@
+import os
+
+fp = open("label.txt")
+
+image_index = []
+
+image_index = fp.readline().split(';')
+
+image_class = fp.readline().split(';')
+
+pwd = os.getcwd()
+
+for i in range(1,len(image_index) + 1):
+	if i < 10:
+		num_0 = '0000'
+	elif i < 100:
+		num_0 = '000'
+	elif i < 1000:
+		num_0 = '00'
+	else:
+		num_0 = '0'  
+
+
+	old = pwd + '/image_' + num_0 + str(i) + '.jpg'
+	new = pwd + '/image_' + num_0 + str(i) + '_class_' + str(int(image_class[i - 1])) + '.jpg'
+	os.rename(old, new)
