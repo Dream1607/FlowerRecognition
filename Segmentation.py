dde3ba906a3608400dc297cd0d99951da4051d5a
RoyXue812 <royxue812@gmail.com>
2015-05-31 13:40:21 +0800

    Segmentation results saved & GrabCut arguments fixed

--- Segmentation.py
+++ Segmentation.py
@@ -5,6 +5,7 @@ import cPickle as pickle
 import numpy as np
 
 import os
+import sys
 import math
 import datetime
 
@@ -49,7 +50,7 @@ def Grab_Cut(img, segmentation_mask = None, save_name = False):
 #################### rect参数需要居中调整
 
         # the coordinates of a rectangle which includes the foreground object in the format (x,y,w,h)
-        rect = (50,50,weight - 150,height - 100)
+        rect = (int(0.15 * weight),int(0.15 * height),int(0.7 * weight),int(0.7 * height))
         cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
     else:
         mask = np.array(segmentation_mask,np.uint8) * 3
@@ -59,12 +60,18 @@ def Grab_Cut(img, segmentation_mask = None, save_name = False):
     result = np.where((mask==2)|(mask==0),0,1).astype('uint8')
 
     if segmentation_mask != None:
-        img = img*result[:,:,np.newaxis]
+        img = img * result[:,:,np.newaxis]
+
+        f = open(save_name + 'segmentation_result.txt', 'w')
+        for row in img.tolist():
+            f.write("%s\n" % row)
+        f.close()
+
         plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
         plt.axis('off')
         plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
                                 wspace=None, hspace=None)
-        plt.savefig(save_name+'_final_result.jpg')
+        plt.savefig(save_name + '_final_result.jpg')
 
     return result
 
@@ -420,14 +427,16 @@ if __name__ == "__main__":
     predicted = SuperPixels_Segmentation_Adjust(Superpixel_Features, Labels)
 
     offset = 0
+
     for image_index, image_name in enumerate(os.listdir(img_folder)):
-        image_path = img_folder + str("/") +image_name
+        image_path = img_folder + str("/") + image_name
         img =  cv2.imread(image_path)
         segments,segments_pixels,segments_label = Label_Super_Pixels(Super_Pixels(image_path),Grab_Cut(img))
         mask = []
         for seg in segments.flatten():
-            mask.append(predicted[offset+seg])
+            mask.append(predicted[offset + seg])
         mask = np.array(mask).reshape(segments.shape).tolist()
+
         Grab_Cut(img, mask,save_name = image_name.split('.')[0] + '_after_')
-        draw(img,mask,save_name = image_name.split('.')[0] + '_after_')
-        offset+=len(segments_label)
+
+        offset += len(segments_label)
